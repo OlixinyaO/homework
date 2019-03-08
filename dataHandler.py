67756764d74d47552798cea43d386febb1ca7a5e
lxy <547182740@qq.com>
2019-03-08 18:01:41 +0800

    add

--- dataHandler.py
+++ dataHandler.py
@@ -2,20 +2,19 @@
 # coding=utf-8
 
 import re
+import csv
+import json
 
 def testData():
     rawList = [
-        "1037656074935984129,北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に… https://t.co/2mEk1Vu1AG,AbeShinzo,2018/09/06 19:57:41",
-        "1037656095412576256,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,dragons_victory,2018/09/06 19:57:46",
-        "1037656153176518656,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,MOPIRA,2018/09/06 19:58:00",
-        "1037656159207940098,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,hayataka11,2018/09/06 19:58:01",
-        "1037656211066241024,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,choki41,2018/09/06 19:58:14",
-        "1037656214199390209,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,the_sintou,2018/09/06 19:58:14",
-        "1037656236269789184,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,oochan2010,2018/09/06 19:58:20",
-        "1037656277290082305,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,umbro47,2018/09/06 19:58:29",
-        "1037656311054327809,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,everhiro,2018/09/06 19:58:37",
-        "1037656342532509696,RT @AbeShinzo: 北海道胆振東部地震によりお亡くなりになられた方々にお悔やみを申し上げるとともに被災されたすべての方々に心よりお見舞いを申し上げます。自衛隊、警察、消防、海上保安庁が現時点で２万１千人、ヘリ５１機、艦船１２隻の態勢で救命・救助活動に取り組んでおり現在…,clarinet3710,2018/09/06 19:58:45",
-            ]
+        "1037830994621911041,RT @taikokoji: こんばんは(こんばんは) 今週9日日曜日に開催予定でした、ラブライブ！サンシャイン!!Aqours クラブ活動 LIVE &amp; FAN MEETING 2018 ユニット対抗全国ツアー 札幌公演が、6日の朝発生した北海道胆振東部地震に伴い、本日未…,Koumt,2018/09/07 07:32:45",
+        "1037831108723765249,RT @PrefHokkaido: ≪　平成30年北海道胆振東部地震に関する情報　≫停電解消戸数の状況は本日9月7日3時現在で、96万4千戸となっています。電力に関する最新情報はこちらからご覧ください。https://t.co/ahe8bGOifd,ah_love_yuki,2018/09/07 07:33:12"]
+    # csv_reader = csv.reader(open("./rawData/Hokkaido.csv", 'r', encoding='UTF-8'))
+    # i = 0
+    # for row in csv_reader:
+    #     if i != 0:
+    #         rawList.append(row)
+    #     i = i + 1
 
     return rawList
 
@@ -38,21 +37,26 @@ def transRawToDict():
             nodesList.append({"name": user_id, "category": 0, "id": user_id.lower()})
         # 1. 人名做节点
         if sourceInfo["ifRaw"] is False:
-            linksList.append({"source": sourceInfo["sourceUser"].lower(), "target": user_id.lower() })
+            linkInfo = json.dumps({"source": sourceInfo["sourceUser"].lower(), "target": user_id.lower() })
+            linksList.append(linkInfo)
 
-        # if sourceUser["ifRaw"]:
-        #     sourceInfo.update({"sourceUser": user_id})
-        # else:
-        #     linksList.append({"source": sourceUser["text"].lower(), "target": user_id.lower() })
-        
-        # textDict.update({sourceInfo["text"]: sourceInfo["sourceUser"]})
+    fp = open("./showData/test.json", 'w', encoding='UTF-8')
+    fp.write(linksList)
+    fp.close()
 
         
 
 def getSourceText(text):
-    sourceUser = re.findall(".*@(.*):.*", text)
-    sourceText = re.findall(".*@.*:(.*)", text)
+    sourceUser = re.findall(".*@(.*)", text.split(':')[0])
     if len(sourceUser) == 0:
-        return {"ifRaw": True, "text": text}
+        return {"ifRaw": True}
     else:
-        return {"ifRaw": False, "text": sourceText, "sourceUser": sourceUser}
+        return {"ifRaw": False, "sourceUser": sourceUser[0]}
+
+transRawToDict()
+
+t1 = "RT @taikokoji: こんばんは(こんばんは) 今週9日日曜日に"
+t2 = "RT @PrefHokkaido: ≪　平成30年北海道胆振東部地震に関"
+
+# getSourceText(t1)
+# getSourceText(t2)
