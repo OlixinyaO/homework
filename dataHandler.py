7c10ff1f88ad39dfc5ae9a3d1c1ac08a6db72f21
OlixinyaO <547182740@qq.com>
2019-03-09 14:37:02 +0800

    完成

--- dataHandler.py
+++ dataHandler.py
@@ -7,14 +7,15 @@ import json
 
 def testData():
     rawList = [
-        "1037830994621911041,RT @taikokoji: こんばんは(こんばんは) 今週9日日曜日に開催予定でした、ラブライブ！サンシャイン!!Aqours クラブ活動 LIVE &amp; FAN MEETING 2018 ユニット対抗全国ツアー 札幌公演が、6日の朝発生した北海道胆振東部地震に伴い、本日未…,Koumt,2018/09/07 07:32:45",
-        "1037831108723765249,RT @PrefHokkaido: ≪　平成30年北海道胆振東部地震に関する情報　≫停電解消戸数の状況は本日9月7日3時現在で、96万4千戸となっています。電力に関する最新情報はこちらからご覧ください。https://t.co/ahe8bGOifd,ah_love_yuki,2018/09/07 07:33:12"]
-    # csv_reader = csv.reader(open("./rawData/Hokkaido.csv", 'r', encoding='UTF-8'))
-    # i = 0
-    # for row in csv_reader:
-    #     if i != 0:
-    #         rawList.append(row)
-    #     i = i + 1
+        # "1037830994621911041,RT @taikokoji: こんばんは(こんばんは) 今週9日日曜日に開催予定でした、ラブライブ！サンシャイン!!Aqours クラブ活動 LIVE &amp; FAN MEETING 2018 ユニット対抗全国ツアー 札幌公演が、6日の朝発生した北海道胆振東部地震に伴い、本日未…,Koumt,2018/09/07 07:32:45",
+        # "1037831108723765249,RT @PrefHokkaido: ≪　平成30年北海道胆振東部地震に関する情報　≫停電解消戸数の状況は本日9月7日3時現在で、96万4千戸となっています。電力に関する最新情報はこちらからご覧ください。https://t.co/ahe8bGOifd,ah_love_yuki,2018/09/07 07:33:12"
+        ]
+    csv_reader = csv.reader(open("./rawData/Hokkaido.csv", 'r', encoding='UTF-8'))
+    i = 0
+    for row in csv_reader:
+        if i != 0:
+            rawList.append(row)
+        i = i + 1
 
     return rawList
 
@@ -24,27 +25,62 @@ def transRawToDict():
     userList = []
     nodesList = []
     linksList = []
+    timedict = {}
+    timeList = []
     for x in rawList:
-        xList = x.split(',')
+        xList = x
+        # xList = x.split(",")
         id = xList[0]
         text = xList[1]
         user_id = xList[2]
         create_time = xList[3]
         sourceInfo = getSourceText(text)
 
+        minute_time = create_time[:-3]
+
+        if minute_time not in timedict:
+            timedict[minute_time] = 1
+            timeList.append(minute_time)
+        else:
+            timedict[minute_time] += 1
+
         if user_id not in userList:
             userList.append(user_id)
-            nodesList.append({"name": user_id, "category": 0, "id": user_id.lower()})
-        # 1. 人名做节点
+            nodeinfo = {"name": user_id, "category": 0, "id": user_id.lower() }
+            nodesList.append(nodeinfo)
+
         if sourceInfo["ifRaw"] is False:
-            linkInfo = json.dumps({"source": sourceInfo["sourceUser"].lower(), "target": user_id.lower() })
+            linkInfo = {"source": sourceInfo["sourceUser"].lower(), "target": user_id.lower() }
             linksList.append(linkInfo)
 
-    fp = open("./showData/test.json", 'w', encoding='UTF-8')
-    fp.write(linksList)
+    
+    timeList.sort()
+    yCountList = []
+    for time in timeList:
+        yCountList.append(timedict[time])
+    
+    saveLineDataInfile(timeList, yCountList)
+    saveNewDataInfile(linksList, nodesList)
+
+def saveNewDataInfile(linksList, nodesList):
+    fp = open("./showData/netData.json", 'w', encoding='UTF-8')
+    fp.write("loadNetData(")
+    fp.write(json.dumps({
+        "type": "force",
+        "categories": ["rawUser"],
+        "links": linksList,
+        "nodes": nodesList}))
+    fp.write(")")
     fp.close()
 
-        
+def saveLineDataInfile(timeList, yCountList):
+    fp = open("./showData/lineChartData.json", 'w', encoding='UTF-8')
+    fp.write("loadLineChartData(")
+    fp.write(json.dumps({
+        "timeList": timeList,
+        "yCountList": yCountList}))
+    fp.write(")")
+    fp.close()      
 
 def getSourceText(text):
     sourceUser = re.findall(".*@(.*)", text.split(':')[0])
@@ -54,9 +90,6 @@ def getSourceText(text):
         return {"ifRaw": False, "sourceUser": sourceUser[0]}
 
 transRawToDict()
-
-t1 = "RT @taikokoji: こんばんは(こんばんは) 今週9日日曜日に"
-t2 = "RT @PrefHokkaido: ≪　平成30年北海道胆振東部地震に関"
 
 # getSourceText(t1)
 # getSourceText(t2)
--- js/dataHandler.js
+++ js/dataHandler.js
@@ -1,22 +1,74 @@
-$(() => {
-    // initNetData()
-})
-
-
+/**
+ * 通过jsonp读取到json文件中处理后的数据
+ * 回调函数，名需要与 src 中一致，而且要与文件地址中名一致。jsonp格式 名称({}) 不然无法获取到对应的文件
+ * @param {*} result 
+ */
 function loadLineChartData(result){
-    //回调函数名称(indexDemo)，需要与 src 中一致，而且要与文件地址中名一致。jsonp格式 名称({})
-  //不然无法获取到对应的文件
-     console.log(result)     //打印 indexDemo.json 中的数据
-     initNet(result)
+    
+     initLine(result)
 }
 
-function loadNetData(webkitDep){
-    //回调函数名称(indexDemo)，需要与 src 中一致，而且要与文件地址中名一致。jsonp格式 名称({})
-  //不然无法获取到对应的文件
-    //  console.log(result)     //打印 indexDemo.json 中的数据
-     
+/**
+ * 通过jsonp读取到json文件中处理后的数据
+ * 回调函数，名称需要与 src 中一致，而且要与文件地址中名一致。jsonp格式 名称({}) 不然无法获取到对应的文件
+ * @param {*} result 
+ */
+function loadNetData(result){
+    
+    initNet(result)
 }
 
+/**
+ * 根据传入数据，初始化折线图
+ * @param {*} webkitDep 
+ */
+function initLine(webkitDep) {
+    var lineChart = echarts.init(document.getElementById('linebox'));
+    let option = {
+        title: {
+            text: "标题",
+            x: 'center'
+        },
+        grid: {
+            bottom: 80
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type : 'line'
+            }
+        },
+        dataZoom: [
+            {
+                type: 'slider',
+                xAxisIndex: 0,
+                filterMode: 'empty'
+            },
+            {
+                type: 'inside',
+                xAxisIndex: 0,
+                filterMode: 'empty'
+            }
+        ],
+        xAxis: {
+            data: webkitDep.timeList
+        },
+        yAxis : [{
+            name: "次数",
+            type : 'value',
+        }],
+        series: [{
+            data: webkitDep.yCountList,
+            type: 'line'
+        }]
+    };
+    lineChart.setOption(option);
+}
+
+/**
+ * 根据传入数据，初始化关系网络图
+ * @param {*} webkitDep 
+ */
 function initNet(webkitDep) {
     var netChart = echarts.init(document.getElementById('netbox'));
 
@@ -34,22 +86,21 @@ function initNet(webkitDep) {
                     formatter: '{b}'
                 }
             },
-            draggable: true,
+            animationDurationUpdate: 1500,
+            animationEasingUpdate: 'quinticInOut',
+            draggable: false,
             data: webkitDep.nodes.map(function (node, idx) {
-                // node.id = idx;
                 return node;
             }),
             categories: webkitDep.categories,
             force: {
-                // initLayout: 'circular'
-                // repulsion: 20,
-                edgeLength: 5,
-                repulsion: 20,
-                gravity: 0.2
+                edgeLength: 3,
+                repulsion: 10,
+                gravity: 0.2,
+                layoutAnimation: false
             },
             edges: webkitDep.links
         }]
     };
-
     netChart.setOption(option);
 }
